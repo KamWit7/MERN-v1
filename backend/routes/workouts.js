d452429c2b2db7391739977023c8b5435ac9bcc4
Kamil Witkowski <53374963+KamWit7@users.noreply.github.com>
2023-02-14 14:05:00 +0100

    contollers p.2

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -3,6 +3,8 @@ const {
   createWorkout,
   getWorkout,
   getWorkouts,
+  deleteWorkout,
+  updateWorkout,
 } = require('../controllers/workoutController')
 
 const router = express.Router()
@@ -15,14 +17,11 @@ router.get('/:id', getWorkout)
 // Post a new Workout
 router.post('/', createWorkout)
 
-router.delete('/', (req, res) => {
-  res.json({ message: 'DELETE a workout' })
-})
+// Delete a workout
+router.delete('/:id', deleteWorkout)
 
 // UPDATE a workout
-router.patch('/', (req, res) => {
-  res.json({ message: 'UPDATE a workout' })
-})
+router.patch('/:id', updateWorkout)
 
 // when -> /api/workout/hello
 // router.get('/hello', () => {})
