9d06e2569256dc0459d79ee871f34224ff7f6a76
KamWit7 <kamilw77@interia.pl>
2023-02-18 12:22:49 +0100

    login setup

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ app.use((req, res, next) => {
 
 // routes
 app.get('/', (req, res) => {
-  res.json({ message: 'Welcome to the app' })
+  res.json({ message: 'Welcome to the app :)' })
 })
 
 // a touch all routs from workout.js
